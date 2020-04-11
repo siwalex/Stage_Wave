3ea807e0f7f8872514938c0b495ff21c4875b7db
Julie97434 <juulie.sibille@gmail.com>
2020-04-11 17:33:02 +0400

    update_histo_run+mau

--- Histo_RUN.m
+++ Histo_RUN.m
@@ -1,6 +1,5 @@
 clear,clc
 
-%ncdisp '2016_03.nc'
 mH4=[];
 mTp4=[];
 Pwm=[];
@@ -16,16 +15,16 @@ for m=1:12     %boucle Annees
 
         %-----Extraire longitude latitute ---------------%
         long=ncread(filename,'longitude');    %réccupération de la matrice des longitudes
-        long=long(38:50);
+        long=long(14:27);
         lat=ncread(filename,'latitude'); 
-        lat=lat(16:28);
+        lat=lat(6:19);
         %--------------------------------------------------------%
 
         %-----Extraire les données du fichier .nc ---------------%
         H0=ncread(filename,'VHM0');     %Hauteur
-        H0=H0(38:50,16:28,:);
+        H0=H0(14:27,6:19,:);
         Tp0=ncread(filename,'VTPK');      %Peak period
-        Tp0=Tp0(38:50,16:28,:);
+        Tp0=Tp0(14:27,6:19,:);
         H1=[];
         Tp1=[];
         for i=1:length(t)
@@ -51,28 +50,60 @@ for m=1:12     %boucle Annees
     mTp3=mean(mTp2,2);
     
     mH4=[mH4 mH3];
-    mTp4=[mTp4 mTp3];
+    mTp4=[mTp4 mTp3];    
     
-    %-------------calcul puissance moyenne------------------%
-    P=mH3.^2.*mTp3;
-    Pw=0.4.*P;
-    
-    Pwm=[Pwm Pw];
-    mPw=reshape(Pw,length(lat),length(long));
-    %--------------------------------------------------------%
-    
+%     %------------------Histogramme-hauteur-------------------%    
+%     figure(m)
+%     subplot(2,1,1)
+%     h=histogram(mH4,50,'Normalization','probability');
+%     tit=sprintf('Distribution mensuelle du %.2d hauteur des vagues pour La Reunion',m);
+%     title(tit);
+%     xlim([1, 3.5]);
+%     hold on
+%     % comptage sur 100 casiers
+%     [N,edges]  = histcounts(mH4,100);
+%     % valeur moyenne des casiers
+%     edges = (edges(1:end-1)+edges(2:end))/2;
+%     % N en cumulé et pourcentage
+%     N = cumsum(N)/sum(N)*100;
+%     % affichage
+%     plotyy(0,0,edges,N)
+%     [N, index] = unique(N); 
+%     yi1 = interp1(N, edges(index), 50);
+%     hold off
+%     %--------------------------------------------------------%
+% 
+%     %------------------Histogramme-Periode-------------------%
+%     subplot(2,1,2)
+%     h=histogram(mTp4,50,'Normalization','probability');
+%     tit=sprintf('Distribution mensuelle du %.2d de la période pour La Reunion',m);
+%     title(tit);
+%     xlim([8, 15]);
+%     hold on
+%     % comptage sur 100 casiers
+%     [N,edges]  = histcounts(mTp4,100);
+%     N(N==0)=1;
+%     % valeur moyenne des casiers
+%     edges = (edges(1:end-1)+edges(2:end))/2;
+%     % N en cumulé et pourcentage
+%     N = cumsum(N)/sum(N)*100;
+%     % affichage
+%     plotyy(0,0,edges,N)
+%     [N, index] = unique(N); 
+%     yi3 = interp1(N, edges(index), 50);
+%     hold off
+%     %--------------------------------------------------------%
 end
-%----------Puissance pour carte annuel----------------------%
-yPw=sum(Pwm,2);
-ymPw=reshape(yPw,length(lat),length(long));
-%--------------------------------------------------------%
 
     
 %------------------Histogramme-hauteur-------------------%    
 figure(14)
-h=histogram(mH4,50,'Normalization','probability');
-tit=sprintf('Distribution hauteur des vagues sur toute la zone');
+subplot(2,1,1)
+h=histogram(mH4,20,'Normalization','probability');
+tit=sprintf('Distribution annuelle de la hauteur des vagues pour La Reunion');
 title(tit);
+xlim([1, 3.5]);
+xlabel('Hauteurs en metre')
 hold on
 % comptage sur 100 casiers
 [N,edges]  = histcounts(mH4,100);
@@ -81,17 +112,23 @@ edges = (edges(1:end-1)+edges(2:end))/2;
 % N en cumulé et pourcentage
 N = cumsum(N)/sum(N)*100;
 % affichage
-plotyy(0,0,edges,N)
+[hAx,~,~]=plotyy(0,0,edges,N);
+ylabel(hAx(1),'Fréquence ') % left y-axis 
+ylabel(hAx(2),'Fréquence cumulé en %') % right y-axis
+set(hAx,{'ycolor'},{'k';'r'})
 [N, index] = unique(N); 
 yi1 = interp1(N, edges(index), 50);
 hold off
 %--------------------------------------------------------%
 
 %------------------Histogramme-Periode-------------------%
-figure(15)
-h=histogram(mTp4,50,'Normalization','probability');
-tit=sprintf('Distribution de la période sur toute la zone');
+subplot(2,1,2)
+h=histogram(mTp4,20,'Normalization','probability');
+tit=sprintf('Distribution annuelle de la période sur toute la zone');
 title(tit);
+xlim([8, 15]);
+xlabel('Periode en s')
+ylabel('Fréquence')
 hold on
 % comptage sur 100 casiers
 [N,edges]  = histcounts(mTp4,100);
@@ -101,15 +138,20 @@ edges = (edges(1:end-1)+edges(2:end))/2;
 % N en cumulé et pourcentage
 N = cumsum(N)/sum(N)*100;
 % affichage
-plotyy(0,0,edges,N)
+[hAx,~,~]=plotyy(0,0,edges,N);
+ylabel(hAx(1),'Fréquence ') % left y-axis 
+ylabel(hAx(2),'Fréquence cumulé en %') % right y-axis
+set(hAx,{'ycolor'},{'k';'r'})
 [N, index] = unique(N); 
 yi3 = interp1(N, edges(index), 50);
 hold off
+s=sprintf('Histo_HTp_MA_run.png');
+saveas(gcf,s);
 %--------------------------------------------------------%
 
-
+%------Puissance moyenne-------------%
 Pf=yi1^2.*yi3;
 Pwf=0.4.*Pf;
-    
+%------------------------------------%    
     
     
